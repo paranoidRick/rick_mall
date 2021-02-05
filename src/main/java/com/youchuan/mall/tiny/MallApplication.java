ffee722e6b81d68f6ae0e501b777de40650f873e
yangyouchuan <1184540833@qq.com>
2021-02-05 16:37:42 +0800

    add springsecurity and update swagger

--- src/main/java/com/youchuan/mall/tiny/MallApplication.java
+++ src/main/java/com/youchuan/mall/tiny/MallApplication.java
@@ -1,8 +1,10 @@
 package com.youchuan.mall.tiny;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.youchuan.mall.tiny.dao")
 @SpringBootApplication
 public class MallApplication {
 
